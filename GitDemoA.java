7f8f9da223074c03abcb293ce61b2ef676fc921e
ashitha123456 <ashithashetty064@gmail.com>
2019-07-22 17:17:33 +0530

    1st commit

---
+++ GitDemoA.java
@@ -0,0 +1,17 @@
+package gitjenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitDemoA {
+	@Test
+	public static void run() {
+		Reporter.log("run()",true);
+	}
+
+
+
+}
+
+
+
